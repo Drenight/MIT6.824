21f51e5c6328cd2d66b80dc919d1ac3a64e38c30
Liu Boyao <liuboyao@Lius-MacBook-Pro.local>
2022-03-24 23:32:06 +0800

    start

--- src/mr/master.go
+++ src/mr/master.go
@@ -1,15 +1,26 @@
 package mr
 
-import "log"
-import "net"
-import "os"
-import "net/rpc"
-import "net/http"
+import (
+	"log"
+	"net"
+	"net/http"
+	"net/rpc"
+	"os"
+	"sync"
+	"time"
+)
 
+type fileStatus int
+
+const fileFree = fileStatus(0)
+const fileDone = fileStatus(1)
 
 type Master struct {
 	// Your definitions here.
-
+	Files         []string
+	NReduce       int
+	FilesStatus   map[int]fileStatus //concurrency
+	MapFileStatus map[int]fileStatus
 }
 
 // Your code here -- RPC handlers for the worker to call.
@@ -24,6 +35,97 @@ func (m *Master) Example(args *ExampleArgs, reply *ExampleReply) error {
 	return nil
 }
 
+func (m *Master) ReportMap(args *CallReportMapArgs, reply *interface{}) error {
+	num := args.FileNum
+	var mutex sync.Mutex
+	mutex.Lock()
+	if m.FilesStatus[num] != fileDone && m.FilesStatus[num] != fileFree {
+		m.FilesStatus[num] = fileDone
+	}
+	mutex.Unlock()
+	return nil
+}
+
+func (m *Master) AssignReduceTask(args interface{}, reply *AssignReduceTaskReply) error {
+	needWork := -1
+	allDone := 1
+	var mutex sync.Mutex
+	for i := 0; i < m.NReduce; i++ {
+		if m.MapFileStatus[i] != fileDone {
+			allDone = 0
+		}
+		if m.MapFileStatus[i] != fileDone && m.MapFileStatus[i] != fileFree {
+			if int(time.Now().Unix())-int(m.MapFileStatus[i]) > 10 {
+				mutex.Lock()
+				if m.MapFileStatus[i] != fileDone {
+					m.MapFileStatus[i] = fileFree
+				}
+				mutex.Unlock()
+			}
+		}
+		if m.MapFileStatus[i] == fileFree {
+			mutex.Lock()
+			if m.MapFileStatus[i] != fileFree {
+				continue
+			}
+			m.MapFileStatus[i] = fileStatus(time.Now().Unix())
+			needWork = i
+			mutex.Unlock()
+			break
+		}
+	}
+}
+
+func (m *Master) AssignMapTask(args interface{}, reply *AssignMapTaskReply) error {
+	needWork := -1
+	allDone := 1
+	var mutex sync.Mutex
+	for i := 0; i < len(m.FilesStatus); i++ {
+		if m.FilesStatus[i] != fileDone {
+			allDone = 0
+		}
+		//release status to free for too long working -- 10s
+		if m.FilesStatus[i] != fileDone && m.FilesStatus[i] != fileFree {
+			if int(time.Now().Unix())-int(m.FilesStatus[i]) > 10 {
+				mutex.Lock()
+				if m.FilesStatus[i] != fileDone {
+					m.FilesStatus[i] = fileFree
+				}
+				mutex.Unlock()
+			}
+		}
+		if m.FilesStatus[i] == fileFree {
+			//上锁写入繁忙状态，并且返回任务号
+			mutex.Lock()
+			if m.FilesStatus[i] != fileFree {
+				continue
+			}
+			m.FilesStatus[i] = fileStatus(time.Now().Unix())
+			needWork = i
+			mutex.Unlock()
+			break
+		}
+	}
+
+	reply.NReduce = m.NReduce
+	reply.FileTotalNum = len(m.Files)
+	if allDone == 1 {
+		reply.FileNum = -2
+		reply.FileName = ""
+		reply.FileStatus = fileDone
+		return nil
+	}
+	reply.FileNum = needWork
+	if needWork == -1 {
+		reply.FileName = ""
+		reply.FileStatus = fileDone
+	} else {
+		reply.FileName = m.Files[needWork]
+		reply.FileStatus = m.FilesStatus[needWork]
+	}
+
+	return nil
+}
 
 //
 // start a thread that listens for RPCs from worker.go
@@ -50,7 +152,6 @@ func (m *Master) Done() bool {
 
 	// Your code here.
 
-
 	return ret
 }
 
@@ -60,10 +161,18 @@ func (m *Master) Done() bool {
 // nReduce is the number of reduce tasks to use.
 //
 func MakeMaster(files []string, nReduce int) *Master {
-	m := Master{}
-
+	m := Master{
+		Files:       files,
+		NReduce:     nReduce,
+		FilesStatus: map[int]fileStatus{},
+	}
+	for i := 0; i < len(files); i++ {
+		m.FilesStatus[i] = fileFree
+	}
+	for i := 0; i < nReduce; i++ {
+		m.MapFileStatus[i] = fileFree
+	}
 	// Your code here.
-
 
 	m.server()
 	return &m
--- src/mr/worker.go
+++ src/mr/worker.go
@@ -1,10 +1,18 @@
 package mr
 
-import "fmt"
-import "log"
-import "net/rpc"
-import "hash/fnv"
-
+import (
+	"encoding/json"
+	"fmt"
+	"hash/fnv"
+	"io/ioutil"
+	"log"
+	"net/rpc"
+	"os"
+	"plugin"
+	"sort"
+	"strconv"
+	"time"
+)
 
 //
 // Map functions return a slice of KeyValue.
@@ -14,7 +22,15 @@ type KeyValue struct {
 	Value string
 }
 
-//
+// for sorting by key.
+type ByKey []KeyValue
+
+// for sorting by key.
+func (a ByKey) Len() int           { return len(a) }
+func (a ByKey) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByKey) Less(i, j int) bool { return a[i].Key < a[j].Key }
+
+// 一个打乱词序的过程？对所有词进行分组，再开个fileName那样的带锁map去调reduce
 // use ihash(key) % NReduce to choose the reduce
 // task number for each KeyValue emitted by Map.
 //
@@ -23,7 +39,6 @@ func ihash(key string) int {
 	h.Write([]byte(key))
 	return int(h.Sum32() & 0x7fffffff)
 }
-
 
 //
 // main/mrworker.go calls this function.
@@ -34,8 +49,22 @@ func Worker(mapf func(string, string) []KeyValue,
 	// Your worker implementation here.
 
 	// uncomment to send the Example RPC to the master.
-	// CallExample()
+	//CallExample()
+	for {
+		time.Sleep(time.Second)
+		mapDone := callAssignMapTask()
+		if mapDone {
+			break
+		}
+	}
 
+	for {
+		time.Sleep(time.Second)
+		reduceDone := callAssignReduceTask()
+		if reduceDone {
+			break
+		}
+	}
 }
 
 //
@@ -61,6 +90,115 @@ func CallExample() {
 	fmt.Printf("reply.Y %v\n", reply.Y)
 }
 
+type tmpFile struct {
+	filePtr    *os.File
+	originName string
+	tmpName    string
+}
+
+func callAssignReduceTask() bool {
+	var args interface{}
+	reply := AssignReduceTaskReply{}
+	rpcSuccess := call("Master.AssignReduceTask", &args, &reply)
+	return false
+}
+
+func callReportMap(fileNum int) bool {
+	args := CallReportMapArgs{FileNum: fileNum}
+	var reply interface{}
+	rpcSuccess := call("Master.ReportMap", &args, &reply)
+	return rpcSuccess
+}
+
+func callAssignMapTask() bool {
+	var args interface{}
+	reply := AssignMapTaskReply{}
+	rpcSuccess := call("Master.AssignMapTask", &args, &reply)
+	if !rpcSuccess {
+		return false
+	}
+
+	fmt.Printf("reply:%+v\n", reply)
+
+	if reply.FileNum == -2 {
+		return true
+	}
+
+	if reply.FileNum == -1 {
+		//no available
+		return false
+	}
+
+	// after finish work remember to check timeStamp , exceed 10 mins shall not be submitted
+	mapf, _ := loadPlugin(os.Args[1])
+	intermediate := []KeyValue{}
+
+	filename := reply.FileName
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatalf("cannot open %v", filename)
+		return false
+	}
+	content, err := ioutil.ReadAll(file)
+	if err != nil {
+		log.Fatalf("cannot read %v", filename)
+		return false
+	}
+	file.Close()
+	kva := mapf(filename, string(content))
+	intermediate = append(intermediate, kva...)
+
+	sort.Sort(ByKey(intermediate))
+
+	var intermediateFileSlice []tmpFile
+	for i := 0; i < reply.NReduce; i++ {
+		oname := "mr-" + strconv.Itoa(reply.FileNum) + "-" + strconv.Itoa(i)
+		ofile, _ := ioutil.TempFile(".", oname) //os.Create(oname)
+		tmp := tmpFile{
+			filePtr:    ofile,
+			originName: oname,
+			tmpName:    ofile.Name(),
+		}
+		intermediateFileSlice = append(intermediateFileSlice, tmp)
+	}
+
+	for i := range intermediate {
+		k, _ := intermediate[i].Key, intermediate[i].Value
+		hashResult := ihash(k) % reply.NReduce
+		//write into file "mr-FileNum-hashResult"
+		enc := json.NewEncoder(intermediateFileSlice[hashResult].filePtr)
+		err := enc.Encode(&intermediate[i])
+		if err != nil {
+			log.Fatalf("can not write json")
+			return false
+		}
+	}
+
+	//successfully run here, which means I need to check time whether to report
+	nowTS := time.Now().Unix()
+	if int(nowTS)-int(reply.FileStatus) > 8 { //for safe, give up this process
+		return false
+	}
+
+	//now this is a successful work
+
+	for i := range intermediateFileSlice {
+		err := os.Rename(intermediateFileSlice[i].tmpName, intermediateFileSlice[i].originName)
+		if err != nil {
+			log.Fatalf("fail to rename file")
+			return false
+		}
+		intermediateFileSlice[i].filePtr.Close()
+	}
+
+	flag := callReportMap(reply.FileNum)
+	if !flag {
+		log.Fatalf("fail to report map")
+	}
+
+	return false
+}
+
 //
 // send an RPC request to the master, wait for the response.
 // usually returns true.
@@ -82,4 +220,23 @@ func call(rpcname string, args interface{}, reply interface{}) bool {
 
 	fmt.Println(err)
 	return false
+}
+
+func loadPlugin(filename string) (func(string, string) []KeyValue, func(string, []string) string) {
+	p, err := plugin.Open(filename)
+	if err != nil {
+		log.Fatalf("cannot load plugin %v", filename)
+	}
+	xmapf, err := p.Lookup("Map")
+	if err != nil {
+		log.Fatalf("cannot find Map in %v", filename)
+	}
+	mapf := xmapf.(func(string, string) []KeyValue)
+	xreducef, err := p.Lookup("Reduce")
+	if err != nil {
+		log.Fatalf("cannot find Reduce in %v", filename)
+	}
+	reducef := xreducef.(func(string, []string) string)
+
+	return mapf, reducef
 }
